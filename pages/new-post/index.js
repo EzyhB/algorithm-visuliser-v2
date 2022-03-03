59201e1342311f335837d08727dd128d73f3cc52
EzyhB <ezyhbmail@gmail.com>
2022-03-03 21:04:08 +0000

    finished the make blogpost page and its sending post requests to backend successfully

--- pages/new-post/index.js
+++ pages/new-post/index.js
@@ -1,7 +1,7 @@
 import { Container } from "@mui/material";
 import React from "react";
-import MakePost from "../components/MakePost";
-import Navbar from "../components/Navbar";
+import MakePost from "../../components/MakePost";
+import Navbar from "../../components/Navbar";
 
 export default function NewPost() {
   return (
