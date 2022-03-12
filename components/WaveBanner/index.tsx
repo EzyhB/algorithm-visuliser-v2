4b4be304b3a7715b4b41190a76a044a109a4eaf6
EzyhB <ezyhbmail@gmail.com>
2022-03-12 16:15:29 +0000

    adjusted wave/connect button

--- components/WaveBanner/index.tsx
+++ components/WaveBanner/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import { AppBar, Box, Button, Container, Typography } from "@mui/material";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { ethers } from "ethers";
 
+import FitbitIcon from "@mui/icons-material/Fitbit";
+import AssuredWorkloadIcon from "@mui/icons-material/AssuredWorkload";
+import ViewInArIcon from "@mui/icons-material/ViewInAr";
+
 import abi from "../../utility/WavePortal.json";
+import { useTheme } from "@mui/material";
 
 declare const window: Window &
   typeof globalThis & {
@@ -13,6 +18,10 @@ declare const window: Window &
 
 export default function WaveBanner() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [isMining, setIsMining] = useState(false);
+  const [isHaveMined, setisHaveMined] = useState(false);
+
+  const theme = useTheme();
 
   const contractAddress = "0xC99181CE0841b5F1DD91f63e47DF4DcA3dAb1e2A";
   const contractABI = abi.abi;
@@ -78,8 +87,12 @@ export default function WaveBanner() {
         const waveTxn = await wavePortalContract.wave();
         console.log("Mining...", waveTxn.hash);
 
+        setIsMining(true);
+
         await waveTxn.wait();
         console.log("Mined -- ", waveTxn.hash);
+
+        setIsMining(false);
 
         count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
@@ -96,7 +109,7 @@ export default function WaveBanner() {
   }, []);
 
   return (
-    <Container>
+    <Container sx={{ height: "200vh" }}>
       <Box sx={{ m: { md: 10, sm: 6, xs: 5 } }} />
       <Typography variant="h2" align="center">
         👋 Hey there!
@@ -105,16 +118,39 @@ export default function WaveBanner() {
         I&apos;m Ezyh, &#38; I&apos;ve just deployed my 1st Etherium Smart
         Contract onto the BlockChain! Exciting!
       </Typography>
-
-      {!currentAccount ? (
-        <Button variant="contained" color="secondary" onClick={connectWallet}>
-          Connect Wallet
-        </Button>
-      ) : (
-        <Button variant="contained" color="secondary" onClick={wave}>
-          Wave at me
-        </Button>
-      )}
+      <Box
+        position="fixed"
+        sx={{
+          display: "flex",
+          bottom: "1rem",
+          right: { md: "1rem", xs: 0 },
+          background: theme.palette.primary.light,
+          height: { md: "6rem", xs: "4rem" },
+          width: { md: "14rem", xs: "100%" },
+          borderRadius: "2rem",
+          alignItems: "center",
+        }}
+      >
+        {!currentAccount ? (
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={connectWallet}
+            sx={{ margin: "0 auto" }}
+          >
+            Connect Wallet
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={wave}
+            sx={{ margin: "0 auto" }}
+          >
+            Wave at me
+          </Button>
+        )}
+      </Box>
     </Container>
   );
 }
