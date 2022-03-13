f137c600d185b5e1ceb1ad32fe155c8b6be0ff70
EzyhB <ezyhbmail@gmail.com>
2022-03-13 12:20:55 +0000

    new contract installed and added input fields

--- components/WaveBanner/index.tsx
+++ components/WaveBanner/index.tsx
@@ -1,12 +1,16 @@
-import { AppBar, Box, Button, Container, Typography } from "@mui/material";
-import React from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { SyntheticEvent } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { ethers } from "ethers";
-
-import FitbitIcon from "@mui/icons-material/Fitbit";
-import AssuredWorkloadIcon from "@mui/icons-material/AssuredWorkload";
-import ViewInArIcon from "@mui/icons-material/ViewInAr";
 
 import abi from "../../utility/WavePortal.json";
 import { useTheme } from "@mui/material";
@@ -18,14 +22,47 @@ declare const window: Window &
   };
 
 export default function WaveBanner() {
+  const [allWaves, setAllWaves] = useState([]);
   const [currentAccount, setCurrentAccount] = useState("");
   const [isMining, setIsMining] = useState(false);
   const [isMined, setIsMined] = useState(false);
 
   const theme = useTheme();
 
-  const contractAddress = "0xC99181CE0841b5F1DD91f63e47DF4DcA3dAb1e2A";
+  const contractAddress = "0x0Eb319DFCC963F866ADdeCcd756f6Ed14326dcE6";
   const contractABI = abi.abi;
+
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const waves = await wavePortalContract.getAllWaves();
+        console.log(waves);
+
+        let cleanWaves = [];
+
+        waves.forEach((el) => {
+          cleanWaves.push({
+            address: el.waver,
+            timestamp: new Date(el.timestamp * 1000),
+            message: el.message,
+          });
+        });
+
+        setAllWaves(cleanWaves);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const walletSecurityCheck = async () => {
     try {
@@ -43,6 +80,7 @@ export default function WaveBanner() {
         const account = accounts[0];
         console.log("Found authorised account! ", account);
         setCurrentAccount(account);
+        getAllWaves();
       } else {
         console.log("No authorized account found");
       }
@@ -70,7 +108,22 @@ export default function WaveBanner() {
     }
   };
 
-  const wave = async () => {
+  // const handleSubmitTest = (e: SyntheticEvent) => {
+  //   e.preventDefault();
+  //   console.log(e);
+  //   const form = e.target as HTMLFormElement;
+  //   const input = form[0] as HTMLInputElement;
+
+  //   const message = input.value;
+
+  //   console.log(message);
+  // };
+  const wave = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const input = form[0] as HTMLInputElement;
+
+    const message = input.value;
     try {
       const { ethereum } = window;
       if (ethereum) {
@@ -85,7 +138,7 @@ export default function WaveBanner() {
         let count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
 
-        const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await wavePortalContract.wave(message);
         console.log("Mining...", waveTxn.hash);
 
         setIsMining(true);
@@ -96,7 +149,7 @@ export default function WaveBanner() {
         setTimeout(() => {
           setIsMined(false);
           setIsMining(false);
-        }, 3000);
+        }, 5000);
 
         count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
@@ -122,10 +175,88 @@ export default function WaveBanner() {
         I&apos;m Ezyh, &#38; I&apos;ve just deployed my 1st Etherium Smart
         Contract onto the BlockChain! Exciting!
       </Typography>
-
-      <MiningTimeline isMining={isMining} isMined={isMined} />
-
-      <Box
+      <Container maxWidth="md">
+        <Grid container>
+          <Grid item md={6} xs={12}>
+            <MiningTimeline isMining={isMining} isMined={isMined} />
+          </Grid>
+          <Grid
+            item
+            md={6}
+            xs={12}
+            sx={{ bgcolor: "grey", alignItems: "center" }}
+          >
+            {!currentAccount ? (
+              <Box
+                sx={{
+                  bgcolor: "rgba(181, 80, 248, 0.15)",
+                  maxWidth: { md: "80%", xs: "90%" },
+                  margin: { md: "1rem 1rem", xs: "auto auto" },
+                  height: { md: "85%", xs: "15rem" },
+                  borderRadius: "5rem",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={connectWallet}
+                  sx={{ margin: "0 auto" }}
+                >
+                  Connect Wallet
+                </Button>
+              </Box>
+            ) : (
+              <Box
+                sx={{
+                  bgcolor: "rgba(181, 80, 248, 0.15)",
+                  maxWidth: { md: "80%", xs: "90%" },
+                  margin: { md: "1rem 1rem", xs: "auto auto" },
+                  height: { md: "85%", xs: "15rem" },
+                  borderRadius: "5rem",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <form
+                  style={{
+                    // backgroundColor: "green",
+                    width: "90%",
+                    margin: "auto auto",
+                    display: "flex",
+                    justifyContent: "center",
+                    flexWrap: "wrap",
+                  }}
+                  onSubmit={(e: SyntheticEvent) => {
+                    wave(e);
+                  }}
+                >
+                  <TextField
+                    fullWidth
+                    multiline
+                    rows={4}
+                    placeholder="Message..."
+                    color="secondary"
+                  />
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    type="submit"
+                    sx={{
+                      margin: "0 auto",
+                      bottom: { md: "-2rem", xs: "-1rem" },
+                    }}
+                  >
+                    Wave at me
+                  </Button>
+                </form>
+              </Box>
+            )}
+          </Grid>
+        </Grid>
+      </Container>
+      {/* <Box
         position="fixed"
         sx={{
           display: "flex",
@@ -137,27 +268,9 @@ export default function WaveBanner() {
           borderRadius: "2rem",
           alignItems: "center",
         }}
-      >
-        {!currentAccount ? (
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={connectWallet}
-            sx={{ margin: "0 auto" }}
-          >
-            Connect Wallet
-          </Button>
-        ) : (
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={wave}
-            sx={{ margin: "0 auto" }}
-          >
-            Wave at me
-          </Button>
-        )}
-      </Box>
+      > */}
+
+      {/* </Box> */}
     </Container>
   );
 }
